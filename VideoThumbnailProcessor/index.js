e8a0b9121ed63af1696b8585a09f6624a44de6ef
Ubuntu <simonw@swlinuxhost.expxh3gqamvebig5jjeglc1paf.px.internal.cloudapp.net>
2020-10-20 03:56:07 +0000

    Updates to support new demo.

--- VideoThumbnailProcessor/index.js
+++ VideoThumbnailProcessor/index.js
@@ -1,6 +1,5 @@
 const generatePreview = require('ffmpeg-generate-video-preview');
 const azurestorage = require('azure-storage');
-const guid = require('node-uuid');
 const fs = require('fs');
 const chokidar = require('chokidar');
 
@@ -8,21 +7,25 @@ module.exports = async function (context, myQueueItem) {
 
     try {
 
-      context.log('Processing Video File: ', myQueueItem);
-
       var blobService = azurestorage.createBlobService(process.env.VIDEOFILESTORAGE);
 
-      var videoFileName = guid.v1() + '-' + myQueueItem;
+      // myQueueItem contains the absolute URL to the blob
+      // The assumption is that the filename is already unique
+      var videoFileName = myQueueItem.split('/')[4];
+
+      context.log('Processing Video File: ', videoFileName);
+
       var videoFilePath = __dirname + '/' + videoFileName;
-      var previewFileName = videoFileName + '_preview.jpg';
+      var previewFileName = videoFileName + '_keyframes.jpg';
       var previewFilePath = __dirname + '/' + previewFileName;
 
       var watcher = chokidar.watch(previewFilePath, {ignored: /^\./, persistent: true, awaitWriteFinish: true, ignoreInitial: true});
 
-      await downloadVideoFile(blobService, myQueueItem, videoFilePath);
+      await downloadVideoFile(blobService, videoFileName, videoFilePath);
 
-      context.log('File Downloaded, starting preview creation.');
+      context.log('File downloaded, starting preview creation.');
 
+      // generate multiple keyframes view of video.
       await generatePreview({
         input: videoFilePath,
         output: previewFilePath,
@@ -32,7 +35,7 @@ module.exports = async function (context, myQueueItem) {
         cols: 10
       });
 
-      await checkForFileAndUpload(context, watcher, blobService, previewFilePath, previewFileName, videoFilePath);
+      await checkForFileAndUpload(context, watcher, blobService, previewFileName, "keyframes");
 
       watcher.close();
 
@@ -62,15 +65,15 @@ const downloadVideoFile = async (azureBlobService, videoFileName, localFileName)
   });
 };
 
-const checkForFileAndUpload = async (context, watcher, blobService, previewFilePath, previewFileName, videoFilePath) => {
+const checkForFileAndUpload = async (context, watcher, blobService, fileName, fileType) => {
 
-  context.log('Done with preview creation, uploading preview file.');
+  context.log(`Done with preview creation, uploading ${fileType} file.`);
 
   return new Promise((resolve, reject) => {
 
-    watcher.on('add', (path) => {
+    watcher.on('add', (fileName) => {
 
-      blobService.createBlockBlobFromLocalFile(process.env.THUMBNAILCONTAINER, previewFileName, previewFilePath, (error, result, response) => {
+      blobService.createBlockBlobFromLocalFile(process.env.THUMBNAILCONTAINER, fileName, fileName, (error, result, response) => {
         if (error) {
             context.log(error);
             reject(error);
